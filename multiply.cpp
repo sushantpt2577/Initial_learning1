e9dc8844fb43de5a3c49166421ced46b6722f3d2
Sushant Prashant Tarapure <sushanttarapure@gmail.com>
2023-07-07 19:13:48 +0530

    added a multiply file to person1/mutiply

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int ans;
+    ans=a*b;
+    cout<<ans<<endl;
+    
+    // cout<<"hello world";
+}
